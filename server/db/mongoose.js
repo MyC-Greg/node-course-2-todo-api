a5c1ecafab697cf38bc20c590fb9a4bb03ce75da
MyC <nedelecgregoire@gmail.com>
2016-12-16 11:44:49 +0100

    heroku depl

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds133398.mlab.com:33398/node-todo-api' || 'mongodb://localhost:27017/TodoApp');
+var MONGODB_URI = 'mongodb://<dbuser>:<dbpassword>@ds133398.mlab.com:33398/node-todo-api';
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
