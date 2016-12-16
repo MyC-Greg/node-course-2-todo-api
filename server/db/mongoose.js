c5eacdd1b644232b4f0997d82433e4bb71ef5b7d
MyC <nedelecgregoire@gmail.com>
2016-12-16 11:36:03 +0100

    add config for heroku deployment 3

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var MONGODB_URI = 'https://git.heroku.com/secure-reaches-47935.git';
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('https://git.heroku.com/secure-reaches-47935.git' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
